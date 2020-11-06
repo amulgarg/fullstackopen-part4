7a1cb12a3b2c274c55f9e0bbb1fe283005899478
Amul Garg <amulg2012@gmail.com>
2020-11-06 22:47:42 +0530

    part 4 - completed exercises 4.10-4.11

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,6 +8,7 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+	if(!request.body.likes) request.body.likes = 0;
 	const blog = new Blog(request.body);
 	const result = await blog.save();
 	response.status(201).json(result);
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -17,6 +17,47 @@ test('there exists an id property', async () => {
 	expect(blogs[0].id).toBeDefined();	
 });
 
+test('a valid blog can be added', async () => {
+	const newBlog = {
+        title: "Biden vs Trump - Continued",
+        author: "Nate Silver",
+        url: "https://fivethirtyeight.com",
+        likes: 200
+	};
+	
+	await api.post('/api/blogs').send(newBlog).expect(201);
+
+	const response = await api.get('/api/blogs');
+
+	console.log('blogs response', response.body);
+	const contents = response.body.map(r => r.title);
+
+	expect(response.body).toHaveLength(blogAPITestHelper.initialBlogs.length + 1);
+
+	expect(contents).toContain(
+		'Biden vs Trump - Continued'
+	);
+});
+
+test('a blog without likes key', async () => {
+	const newBlog = {
+        title: "Biden vs Trump - Continued",
+        author: "Nate Silver",
+        url: "https://fivethirtyeight.com"
+	};
+	
+	await api.post('/api/blogs').send(newBlog).expect(201);
+
+	const response = await api.get('/api/blogs');
+
+	console.log('blogs response', response.body);
+	const blog = response.body.find(r => r.title === newBlog.title);
+
+	expect(response.body).toHaveLength(blogAPITestHelper.initialBlogs.length + 1);
+
+	expect(blog.likes).toBe(0);
+});
+
 beforeEach(async () => {
 	await Blog.deleteMany({});//empty the collection
 	console.log('cleared')
