018ba7c3d715fdc5f3376478e0db8d52f24eda92
Amul Garg <amulg2012@gmail.com>
2020-11-06 12:30:05 +0530

    part 4 - completed exercises 4.5

--- tests/list.test.js
+++ tests/list.test.js
@@ -61,3 +61,60 @@ describe('total likes', () => {
 
 	});
 });
+
+
+describe('favourite blog', () => {
+	test('when list has multiple blogs, function should find the favorite blog', () => {
+		const blogs = [
+			{
+				_id: "5fa4d058af79801502275062",
+				title: "Hello World!",
+				author: "Amul Garg",
+				url: "https://amulsblog.com",
+				likes: 100,
+				__v: 0
+			},
+			{
+				_id: "5fa4d086af79801502275063",
+				title: "Pandemic Misery :(",
+				author: "Amul Garg",
+				url: "https://amulsblog.com/pandemic-misery-024",
+				likes: 200,
+				__v: 0
+			}
+		];
+
+		expect(listHelper.favoriteBlog(blogs)).toEqual(blogs[1]);
+
+	});
+ 	test('when list has only one blog, equals that', () => {
+
+		const blogs = [
+			{
+				_id: "5fa4d058af79801502275062",
+				title: "Hello World!",
+				author: "Amul Garg",
+				url: "https://amulsblog.com",
+				likes: 100,
+				__v: 0
+			}
+		];
+
+		expect(listHelper.favoriteBlog(blogs)).toEqual(blogs[0]);
+
+	});
+	
+	test('when list has 0 blogs, equals null', () => {
+		const blogs = [];
+
+		expect(listHelper.favoriteBlog(blogs)).toBe(null);
+
+	});
+
+	test('when list has null blogs, equals null', () => {
+		const blogs = null;
+
+		expect(listHelper.favoriteBlog(blogs)).toBe(null);
+
+	});
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,22 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, item) => sum + item.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+    if(!blogs || blogs.length == 0) return null;
+    if(blogs.length == 1) return blogs[0];
+
+    let favBlog = blogs[0];
+    blogs.forEach(blog => {
+        if(blog.likes > favBlog.likes){
+            favBlog = blog;
+        } 
+    });
+
+    return favBlog;
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
