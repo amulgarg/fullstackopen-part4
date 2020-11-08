6700fc2dc63996088c713cacd5be117b46802264
Amul Garg <amulg2012@gmail.com>
2020-11-08 20:46:42 +0530

    part 4 - completed exercises 4.19-4.19

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware');
 //controllers
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
+const loginRouter = require('./controllers/login');
 
 const mongoUrl = config.mongodbURI;
 
@@ -18,6 +19,7 @@ app.use(express.json())
 
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.errorHandler);
 
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', {blogs: 0});
@@ -15,9 +16,30 @@ blogsRouter.delete('/:id', async (request, response) => {
 	response.status(204).end();
 })
 
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7)
+	}
+	return null
+}
+
 blogsRouter.post('/', async (request, response) => {
 	if(!request.body.likes) request.body.likes = 0;
-	const user = await User.findOne(); //pick any user
+
+	const token = getTokenFrom(request);
+	let decodedToken = null;
+
+	try{
+		decodedToken = jwt.verify(token, process.env.SECRET);
+		if (!token || !decodedToken.id) {
+			return response.status(401).json({ error: 'token missing or invalid' })
+		}
+	} catch(error){
+		return response.status(401).json({ error: 'token missing or invalid' });
+	};
+		
+	const user = await User.findById(decodedToken.id)
 	console.log('user', user);
 	const newBlogPayload = {
 		...request.body,
